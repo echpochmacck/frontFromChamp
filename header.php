1ab5bc18993bca48890e0bfd4208f20a1c494e74
user <user@localhost.localdomain>
2025-05-26 10:35:35 +0300

    first page

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/base.css">
+    <link rel="stylesheet" href="styles/css.css">
+
+</head>
+<body>
+
+<header>
+    <div class="container p-20">
+        <div class="logo">
+            <img src="" alt="logo">
+        </div>
+        <ul class="d-flex a-c g-20 fw-w">
+            <li><a href="/#search">Поиск</a></li>
+            <li><a href="login.php">Войти</a></li>
+            <li><a href="profile.php">Личный кабинет</a></li>
+            <li><a href="catalog.php">Каталог игр</a></li>
+        </ul>
+    </div>
+</header>
